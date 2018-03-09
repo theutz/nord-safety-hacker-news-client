97a04861be0fdb0dd256ced2940f9358861368f1
Michael Utz <michael@theutz.com>
2018-03-09 16:53:04 +0200

    chore: setup best stories component

---
+++ src/components/BestStories/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class BestStories extends React.Component<object, object> {
+  render() {
+    const props = this.props;
+
+    return <div {...props} />;
+  }
+}
+
+export default BestStories;
--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import { Route } from 'react-router-dom';
+//
+import BestStories from '../BestStories';
 
 class MainContent extends React.Component<object, object> {
   render() {
     const props = this.props;
 
-    return <section className="section" {...props} />;
+    return (
+      <section className="section" {...props}>
+        <Route path="/" component={BestStories} />
+      </section>
+    );
   }
 }
 
