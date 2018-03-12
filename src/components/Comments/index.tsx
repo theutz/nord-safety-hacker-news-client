084874719a6b8d052b628315f0215bcf61470f3e
Michael Utz <michael@theutz.com>
2018-03-12 14:55:10 +0200

    feat: basic comment layout

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -6,6 +6,14 @@ export interface CommentsProps {
   story: Story;
 }
 
-const Comments = ({ }: CommentsProps) => <div>Comments</div>;
+const Comments = ({ }: CommentsProps) => (
+  <div className="container">
+    <div className="columns">
+      <div className="column">
+        <h2 className="title">Comments</h2>
+      </div>
+    </div>
+  </div>
+);
 
 export default Comments;
